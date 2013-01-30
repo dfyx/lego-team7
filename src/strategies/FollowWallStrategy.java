666308b829324112d1d3d544a8556b38f2815071
sbayer <bayer@sunsteps.org>
2013-01-30 21:29:12 +0100

    * better steering values

--- src/strategies/FollowWallStrategy.java
+++ src/strategies/FollowWallStrategy.java
@@ -12,9 +12,10 @@ public class FollowWallStrategy extends Strategy {
 	/**
 	 * desired distance to wall (in mm)
 	 */
-	private int referenceValue = 100;
+	private int referenceValue = 150;
 
-	private int speed = 1000;
+	private static final int MAX_SPEED = 500;
+	private int speed = MAX_SPEED;
 
 	private enum HeadOn {
 		RIGHT_SIDE, LEFT_SIDE
@@ -25,37 +26,41 @@ public class FollowWallStrategy extends Strategy {
 	/**
 	 * Turn on max speed outside of +- 5cm corridor 50*_20_ = 1000
 	 */
-	private static final int LINEAR_FACTOR = 20;
-	
+	private static final int LINEAR_FACTOR = 10;
+
+	private static final int LINEAR_FACTOR_MOVE_AWAY = 7;
+	private static final int LINEAR_FACTOR_MOVE_TOWARDS = 5;
+
 	UltrasonicSensor realSensor;
 
 	/**
 	 * distance to wall (in mm)
 	 */
 	private int actualValue;
-	
+
 	public FollowWallStrategy() {
-		
+
 	}
 
 	protected void doInit() {
 		realSensor = new lejos.nxt.UltrasonicSensor(Platform.ULTRASONIC_PORT);
 		realSensor.setMode(UltrasonicSensor.MODE_CONTINUOUS);
-		headOn = HeadOn.RIGHT_SIDE;
+		headOn = HeadOn.LEFT_SIDE;
 	}
 
 	protected void doRun() {
 		// TODO SB set to real sensor data
 		// read data and convert to mm
-		actualValue = realSensor.getDistance()*10;
+		actualValue = realSensor.getDistance() * 10;
 
 		int direction = getMotorSpeed();
 
 		if (headOn == HeadOn.LEFT_SIDE)
 			direction = -direction;
-		
-		System.out.println("IST/SOLL: " + actualValue + " / " + referenceValue + " -> " + direction);
-		
+
+		System.out.println("IST/SOLL: " + actualValue + " / " + referenceValue
+				+ " -> " + direction);
+
 		ENGINE.move(speed, direction);
 	}
 
@@ -65,7 +70,14 @@ public class FollowWallStrategy extends Strategy {
 	 * @return positive = move to wall; negative = move away from wall
 	 */
 	private int getMotorSpeed() {
-		int linearValue = (actualValue - referenceValue) * LINEAR_FACTOR;
+		int diff = (actualValue - referenceValue);
+		int linearValue = 0;
+		// move to wall
+		if (linearValue > 0)
+			linearValue = diff * LINEAR_FACTOR_MOVE_TOWARDS;
+		else
+			linearValue = diff * LINEAR_FACTOR_MOVE_AWAY;
+
 		return Utils.clamp(linearValue, -1000, 1000);
 	}
 }
