af67bb204ad5b90804148d0b869f50085d83ab92
sbayer <bayer@sunsteps.org>
2013-01-30 13:48:51 +0100

    * add clocked main loop

--- src/Loop.java
+++ src/Loop.java
@@ -3,9 +3,39 @@ import static robot.Platform.SENSORS;
 import lejos.util.Delay;
 import sensors.Sensor;
 import strategies.Strategy;
+import utils.Utils;
 
 public class Loop {
+	/**
+	 * Time the main loop needs per cycle (in ms)
+	 */
+	private static int LOOP_TIME = 10;
+	
+	/**
+	 * Main loop
+	 * Only run this once!
+	 * 
+	 */
+	public void loop() {
+		Utils.resetTimer();
+		int lastEndTime = 0;
 		
+		boolean run = true;
+		while(run) {
+			System.out.println("lastEnd: " + lastEndTime + ", " + "current: " + Utils.getSystemTime());
+			// ----------------------
+			
+			// TODO your code here
+			
+			// ----------------------
+			// check when to perform the next cycle
+			lastEndTime += LOOP_TIME;
+			if(lastEndTime > Utils.getSystemTime())
+				Delay.msDelay(lastEndTime - Utils.getSystemTime());
+		}
+	}
+	
+	// TODO delete?
 	public void runStrategies(Strategy strategy) {
 		strategy.init();
 		
--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,7 +1,27 @@
 package utils;
 
+import lejos.util.Stopwatch;
+
 public class Utils {
+	private static Stopwatch watch = new Stopwatch();
+	
 	public static int clamp(int value, int min, int max) {
 		return Math.min(max,Math.max(min,value));
+	}
+	
+	/**
+	 * Only call once on program start!
+	 */
+	public static void resetTimer() {
+		watch.reset();
+	}
+	
+	/**
+	 * Counts since start of main loop.
+	 * Only runs for 25 days :-P
+	 * @return system time in ms
+	 */
+	public static int getSystemTime() {
+		return watch.elapsed();
 	}
 }
