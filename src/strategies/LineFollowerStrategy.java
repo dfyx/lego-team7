8249e81a64082555268a3c79a33842bcd949696b
Markus Jung <markus.jung@stud.uni-karlsruhe.de>
2013-01-31 15:25:28 +0100

    Some cleanup and debugging instrumentations for line follower

--- src/strategies/LineFollowerStrategy.java
+++ src/strategies/LineFollowerStrategy.java
@@ -2,15 +2,24 @@ package strategies;
 
 import static robot.Platform.ENGINE;
 import static robot.Platform.LIGHT_SENSOR;
+import utils.Utils;
 
 public class LineFollowerStrategy extends Strategy {
 
-    private static final int MOVE_SPEED = 400;
+    private int speed = 400;
     private static final double EXP_FACTOR = 1.0 / 60000;
     private static final double LINEAR_FACTOR = 0.6;
 
+    public StrategyFollowLine(final int motorSpeed) {
+        if (motorSpeed < 0 || motorSpeed > 1000) {
+            throw new IllegalArgumentException("motorSpeed out of range");
+        }
+        
+        this.speed = motorSpeed;
+    }
+    
     protected void doInit() {
-
+        LIGHT_SENSOR.setFloodlight(true);
     }
 
     protected void doRun() {
@@ -18,8 +27,23 @@ public class LineFollowerStrategy extends Strategy {
 
         final int linear = (int) (LINEAR_FACTOR * error);
         final int exponential = (int) (error * error * error * EXP_FACTOR);
-        final int out = Math.min(1000, Math.max(-1000, linear + exponential));
+        final int out = Utils.clamp(linear + exponential, -1000, 1000);
 
-        ENGINE.move(MOVE_SPEED, out);
+        System.out.println("err: " + error + " lin: " + linear + " exp: "
+                + exponential + " out: " + out);
+
+        ENGINE.move(speed, out);
+    }
+    
+    public int getSpeed() {
+        return speed;
+    }
+    
+    public void setSpeed(int motorSpeed) {
+        if (motorSpeed < 0 || motorSpeed > 1000) {
+            throw new IllegalArgumentException("motorSpeed out of range");
+        }
+        
+        speed = motorSpeed;
     }
 }
