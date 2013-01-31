4c9af9df3fd2456e789f03f308cb8f04990fa479
Markus Jung <markus.jung@stud.uni-karlsruhe.de>
2013-01-31 14:34:14 +0100

    Add new light sensor calibration routine

--- src/strategies/LightCalibrationStrategy.java
+++ src/strategies/LightCalibrationStrategy.java
@@ -2,77 +2,103 @@ package strategies;
 
 import static robot.Platform.ENGINE;
 import static robot.Platform.LIGHT_SENSOR;
-import lejos.nxt.LCD;
 
+import java.util.Arrays;
+
+import utils.Utils;
+
+/**
+ * A strategy to calibrate the light sensor.
+ * 
+ * Concept: The strategy samples measurments during a fixed time while moving.
+ * The largest and the smalles samples will be dropped, a portion of the largest/
+ * smalles remaining samples will be used to calculate a mean value of the
+ * black-point and white-point.
+ * 
+ * @author markus
+ */
 public class LightCalibrationStrategy extends Strategy {
 
-	final double LINEFOUND_FACTOR = 5;
-	final int LINEFOUND_COUNT = 20;
-	final int MOVE_MAX_SPEED = 600;
+    /** Time to sample light sensor. */
+	private static final int SEARCH_TIME = 5 * 1000;
+	/** Driving speed while sampling light sensor. */
+	private static final int BASE_SPEED = 500;
+	/** Number of samples to collect. */
+	private static final int SAMPLE_SIZE = 200;
+	/** Number of largest/smallest samples to ignore. */
+	private static final int DROPPED_SAMPLES = 5;
+    /**
+     * Number of largest/smallest remaining samples to use for black-/whitePoint
+     * calculation.
+     */
+	private static final int ACCEPTED_SAMPLES = 15;
+	/** Time per sample. */
+	private static final int SAMPLE_TIME = SEARCH_TIME / SAMPLE_SIZE;
 
-	private int cycle = 0;
-	
-	private int sum;
-	private int count;
-	private int baseLight;
-	private int minLight;
-	private int maxLight;
-	private int lineValueCount;
+	/** Interval start timestamp */
+	private int startTime = 0;
+	/** The number of currently collected samples */
+	private int sampleCount = 0;
+    /** Collected samples */
+	private int samples[] = new int[SAMPLE_SIZE];
 
 	protected void doInit() {
+	    LIGHT_SENSOR.setFloodlight(true);
 		// Switch off calibration
 		LIGHT_SENSOR.resetCalibration();
-		LIGHT_SENSOR.setFloodlight(false);
-		cycle = 0;
+		
+		startTime = Utils.getSystemTime();
+		
+		ENGINE.move(BASE_SPEED);
 	}
 
-	protected void doRun() {
-		++cycle;
-		
-		if(cycle<10) {
-		} else if(cycle==10) {
-			ENGINE.move(100);
-			sum=0;
-			count=0;
-		} else if(cycle<=110) {
-			sum+=LIGHT_SENSOR.getRawValue();
-			++count;
-		} else if(cycle==120) {
-			baseLight = sum/count;
-			LIGHT_SENSOR.setFloodlight(true);
-		} else if(cycle<130) {
-		} else if(cycle==130) {
-			minLight = Integer.MAX_VALUE;
-			maxLight = Integer.MIN_VALUE;
-			lineValueCount = 0;
-			ENGINE.move(MOVE_MAX_SPEED);
-		} else if (lineValueCount < LINEFOUND_COUNT) {
-			int value = LIGHT_SENSOR.getRawValue();
-			if (value < minLight)
-				minLight = value;
-			if (value > maxLight)
-				maxLight = value;
-			final int normalizedMin = minLight - baseLight;
-			final int normalizedMax = maxLight - baseLight;
-			final int normalizedValue = value - baseLight;
-			if (LINEFOUND_FACTOR * normalizedValue < normalizedMax
-					|| LINEFOUND_FACTOR * normalizedMin < normalizedValue)
-				++lineValueCount;
-			else if (lineValueCount > 1)
-				lineValueCount -= 2;
-			double speedFactorMin = 1 - ((double) normalizedValue)
-					/ (5 * normalizedMin);
-			double speedFactorMax = 1 - ((double) normalizedMax / (5 * normalizedValue));
-			double speedFactor = Math.min(speedFactorMin, speedFactorMax);
-			ENGINE.move((int) (speedFactor * MOVE_MAX_SPEED));
-			LCD.drawString("min: " + (minLight - baseLight) + "   ", 0, 2);
-			LCD.drawString("max: " + (maxLight - baseLight) + "   ", 0, 3);
-			LCD.drawString("value: " + (value - baseLight) + "   ", 0, 4);
-		} else {
-			ENGINE.stop();
-			// Set calibration
-			LIGHT_SENSOR.calibrate(minLight, maxLight);
-			setFinished();
-		}
+	protected void doRun() {	    
+	    if (elapsedTime() > SAMPLE_TIME) {
+	        resetTime();
+	        
+	        final int sampleValue = LIGHT_SENSOR.getRawValue();
+	        samples[sampleCount++] = sampleValue;
+	        
+	        System.out.println("Sample " + sampleCount + ": " + sampleValue);
+	        
+	        if (sampleCount == SAMPLE_SIZE) {
+	            Arrays.sort(samples);
+
+                int blackPoint = 0;
+                int whitePoint = 0;
+                
+	            for (int i = DROPPED_SAMPLES; i < DROPPED_SAMPLES + ACCEPTED_SAMPLES; i++) {
+	                blackPoint += samples[i];
+	                whitePoint += samples[SAMPLE_SIZE - i - 1];
+	            }
+	            
+	            blackPoint /= ACCEPTED_SAMPLES;
+	            whitePoint /= ACCEPTED_SAMPLES;
+	            
+	            LIGHT_SENSOR.calibrate(blackPoint, whitePoint);
+	            
+                System.out.println("Min: " + samples[0] + " Max: "
+                        + samples[SAMPLE_SIZE - 1] + " bp: " + blackPoint
+                        + " wp: " + whitePoint);
+	            
+	            setFinished();
+	        }   
+	    }
+	}
+	
+	/**
+	 * Reset interval timer.
+	 */
+	private void resetTime() {
+	    startTime = Utils.getSystemTime();
+	}
+	
+	/**
+	 * Get the elapsed time since last interval timer reset.
+	 * 
+	 * @return the elapsed time in ms
+	 */
+	private int elapsedTime() {
+	    return Utils.getSystemTime() - startTime;
 	}
 }
