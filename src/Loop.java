a76dce15e70266a2f75d641bbf5c1c7a1fd51203
Sebastian Meßmer <messmer@enspire-it.de>
2013-02-02 17:16:00 +0100

    Main loop written

--- src/Loop.java
+++ src/Loop.java
@@ -9,64 +9,69 @@ public class Loop extends Thread {
 	 * Time the main loop needs per cycle (in ms)
 	 */
 	private static int LOOP_TIME = 10;
-	
+
 	private Strategy strategy;
-	
+
 	/**
 	 * Counts main loop cycles
 	 */
 	private int numCycles = 0;
-	
+
 	private boolean isRunning = false;
 	private boolean abort = false;
-	
+
 	public Loop(Strategy strategy) {
-		this.strategy=strategy;
+		this.strategy = strategy;
 	}
-	
+
 	public void abort() {
 		abort = true;
 	}
-	
+
 	public boolean isRunning() {
 		return isRunning;
 	}
-	
+
 	/**
-	 * Main loop
-	 * Only run this once!
+	 * Main loop Only run this once!
 	 * 
-	 * @param strategy The strategy to perform
+	 * @param strategy
+	 *            The strategy to perform
 	 * 
 	 */
 	public void run() {
-		isRunning = true;
-		
-		Utils.resetTimer();
-		numCycles = 0;
-		
-		strategy.init();
-		
-		int lastEndTime = Utils.getSystemTime();
-		
-		while(strategy.isRunning() && !abort) {
-			// poll sensors
-			Platform.poll();
-			
-			// run strategy and commit changes
-			strategy.run();
+		try {
+			isRunning = true;
+
+			Utils.resetTimer();
+			numCycles = 0;
+
+			strategy.init();
+
+			int lastEndTime = Utils.getSystemTime();
+
+			while (strategy.isRunning() && !abort) {
+				// poll sensors
+				Platform.poll();
+
+				// run strategy and commit changes
+				strategy.run();
+				ENGINE.commit();
+
+				// check when to perform the next cycle
+				lastEndTime += LOOP_TIME;
+				if (lastEndTime > Utils.getSystemTime())
+					Delay.msDelay(lastEndTime - Utils.getSystemTime());
+
+				numCycles++;
+			}
+			isRunning = false;
+		} finally {
+			ENGINE.stop();
 			ENGINE.commit();
-			
-			// check when to perform the next cycle
-			lastEndTime += LOOP_TIME;
-			if(lastEndTime > Utils.getSystemTime())
-				Delay.msDelay(lastEndTime - Utils.getSystemTime());
-			
-			numCycles++;
 		}
-		isRunning=false;
 	}
-	
+
 	public int getNumCycles() {
 		return numCycles;
 	}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import lejos.nxt.Button;
 import lejos.util.Delay;
 import robot.Platform;
-import strategies.TestStrategy;
+import strategies.LineFollowerStrategy;
+import strategies.Strategy;
 
 /**
  * @author markus
@@ -10,18 +11,32 @@ import strategies.TestStrategy;
 
 public class Main {
 
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
 		new Platform();
-		
-		while(true) {		
-			TestStrategy test = new TestStrategy();
-			Loop loop = new Loop(test);
-			loop.start();
-			Button.waitForAnyPress();
-			loop.abort();
-			Button.waitForAnyPress();
-			while(loop.isRunning())
-				Delay.msDelay(100);
+
+		while (!Platform.HEAD.isCalibrating())
+			Delay.msDelay(1);
+		while (Platform.HEAD.isCalibrating())
+			Delay.msDelay(500);
+		System.out.println("Calibrated");
+		System.out.flush();
+
+		while (true) {
+			try {
+				Strategy test = new LineFollowerStrategy(300);
+				Loop loop = new Loop(test);
+				loop.start();
+				Button.waitForAnyPress();
+				loop.abort();
+				Platform.ENGINE.stop();
+				Platform.ENGINE.commit();
+				Button.waitForAnyPress();
+				while (loop.isRunning())
+					Delay.msDelay(100);
+			} finally {
+				Platform.ENGINE.stop();
+				Platform.ENGINE.commit();
+			}
 		}
 	}
 }
