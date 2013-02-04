6f53e2b6c6e27986b772a96803350d9f31f119f7
sbayer <bayer@sunsteps.org>
2013-02-04 01:12:44 +0100

    * frame for avoiding obstacles

--- src/strategies/wall_follower/without_sweeping/collision/FollowCollisionStrategy.java
+++ src/strategies/wall_follower/without_sweeping/collision/FollowCollisionStrategy.java
@@ -5,6 +5,19 @@ import strategies.wall_follower.without_sweeping.DetectCollisionStrategy;
 
 public class FollowCollisionStrategy extends ChildStrategy {
 	DetectCollisionStrategy collisionStrategy;
+	
+	private enum State {
+		START,
+		DRIVE,
+		COLLIDE,
+		STAND,
+		DRIVE_BACK,
+		TURN_HEAD_FORWARD,
+		SEARCH_OBSTACLE, // look forward, if obstacle is there
+		AVOID_OBSTACE, // turn left until obstacle goes out of side
+		TURN_HEAD_SIDEWAYS,
+		SEARCH_WALL
+	}
 
 	public FollowCollisionStrategy(int valueCount, int sensitivity) {
 		collisionStrategy = new DetectCollisionStrategy(valueCount, sensitivity);
